d7b9713e8ebc3044b88187702b3b96bb521df286
Sebastiaan Verplancke <sebastiaan@contract.fit>
2024-12-30 02:38:18 +0100

    update path

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = withBundleAnalyzer({
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '/plantative_website',
+  basePath: '',
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     fontSize: {
       xs: '0.75rem',
