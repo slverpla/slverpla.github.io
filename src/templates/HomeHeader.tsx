0a07c237dbbadb0c94135c9aefc9d98d2d136f4d
Sebastiaan Verplancke <sebastiaan@contract.fit>
2024-12-30 11:12:36 +0100

    fix

--- src/templates/HomeHeader.tsx
+++ src/templates/HomeHeader.tsx
@@ -9,7 +9,7 @@ const HomeHeader = () => (
   <>
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<LogoWithText xl />}>
-        <div className="pr-0 md:pr-8">
+        <div className="pr-1 md:pr-8">
           <ul className="flex items-center space-x-8">
             <li className="hidden md:block">
               <Link href="/" className="underline">
