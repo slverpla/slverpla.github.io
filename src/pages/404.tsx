52251e106d32d761f3d4fff78917f178d7de0b2e
Sebastiaan Verplancke <sebastiaan@contract.fit>
2024-12-31 14:15:12 +0100

    update 404 redirects

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,18 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
+const isMobile = () => {
+  if (typeof window === 'undefined') return false;
+  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+};
+
 export default function Custom404() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/');
+    if (!isMobile()) {
+      router.replace('/');
+    }
   }, [router]);
 
   return null;
