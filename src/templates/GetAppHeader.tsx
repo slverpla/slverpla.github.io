3e0a508bd7d328b4cf53cf6046c879544928230e
Sebastiaan Verplancke <sebastiaan@contract.fit>
2025-01-01 13:53:56 +0100

    add home button back

--- src/templates/GetAppHeader.tsx
+++ src/templates/GetAppHeader.tsx
@@ -9,7 +9,7 @@ const GetAppHeader = () => (
     <NavbarTwoColumns logo={<Logo xl />}>
       <div className="pr-0 md:pr-8">
         <ul className="flex items-center space-x-8">
-          <li className="hidden md:block">
+          <li>
             <Link href="/">Home</Link>
           </li>
           <li className="hidden md:block">
