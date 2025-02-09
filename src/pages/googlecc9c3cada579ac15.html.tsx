5f2a74e352582a2776c98978453c95af0921bf35
Sebastiaan Verplancke <sebastiaan@contract.fit>
2025-02-09 17:59:48 +0100

    make config work

--- src/pages/googlecc9c3cada579ac15.html.tsx
+++ src/pages/googlecc9c3cada579ac15.html.tsx
@@ -1,11 +1,13 @@
-// Add correct content type header
-import type { GetServerSidePropsContext } from 'next';
+export const config = {
+  unstable_runtimeJS: false,
+};
 
 export default function GoogleVerification() {
   return 'google-site-verification: googlecc9c3cada579ac15.html';
 }
 
-export function getServerSideProps({ res }: GetServerSidePropsContext) {
-  res.setHeader('Content-Type', 'text/html');
-  return { props: {} };
+export function getStaticProps() {
+  return {
+    props: {},
+  };
 }
