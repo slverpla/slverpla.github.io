653895fd4d1c384b0fed9cf3de50247577911fcb
Sebastiaan Verplancke <sebastiaan@contract.fit>
2025-02-09 17:20:01 +0100

    add verification

---
+++ src/pages/googlecc9c3cada579ac15.html.tsx
@@ -0,0 +1,9 @@
+export default function GoogleVerification() {
+  return 'google-site-verification: googlecc9c3cada579ac15.html';
+}
+
+export const getStaticProps = () => {
+  return {
+    props: {},
+  };
+};
