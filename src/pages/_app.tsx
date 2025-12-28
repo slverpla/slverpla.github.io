b33744b81086e7ec7c78c32608724000aec6e972
Sebastiaan Verplancke <sebastiaanverplancke@gmail.com>
2025-12-28 16:28:05 +0100

    add snowfall

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import Snowfall from 'react-snowfall';
 
 import { LanguageProvider } from '../context/LanguageContext';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <LanguageProvider>
+    <Snowfall />
     <Component {...pageProps} />
   </LanguageProvider>
 );
